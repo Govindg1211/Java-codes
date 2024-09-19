63a50db9a52f1e10621c9b783cffbd86d5bb0142
Govind Gupta <govind.15986@stu.upes.ac.in>
2024-09-19 09:57:39 +0530

    code done in class

---
+++ code.java
@@ -0,0 +1,41 @@
+// Define the interface
+interface MathOperations {
+    double add(double num1, double num2);
+    double subtract(double num1, double num2);
+    double multiply(double num1, double num2);
+    double divide(double num1, double num2);
+}
+
+// Implement the interface
+public class Calculator implements MathOperations {
+    @Override
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    @Override
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public double divide(double num1, double num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("Cannot divide by zero!");
+        }
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println("2 + 3 = " + calculator.add(2, 3));
+        System.out.println("5 - 2 = " + calculator.subtract(5, 2));
+        System.out.println("4 * 5 = " + calculator.multiply(4, 5));
+        System.out.println("10 / 2 = " + calculator.divide(10, 2));
+    }
+}
