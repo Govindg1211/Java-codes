077c5bb1f9ee33be1e35ac5968675fbbe7e7174c
Govind Gupta <govind.15986@stu.upes.ac.in>
2024-09-24 22:30:18 +0530

    say hi

---
+++ hello.java
@@ -0,0 +1,15 @@
+class hello{
+    public static void main(String[] args) {
+        //String str = "             Java Class java          ";
+        //String str2 = str.trim();
+        String str = String.join(", ","java","python","C","C++");
+        //System.out.println(str.length());
+        //System.out.println(str.substring(2,3));
+        //System.out.println(str.concat("hello"));
+        //System.out.println(str+2);
+        //System.out.println(str.concat("2"));
+        //System.out.println(str.replace("java","python"));
+        System.out.println(str);
+        //System.out.println(str2);
+    }
+}
