5006eed74c73f54d61ddc359e25a791e306a74dc
Govind Gupta <govind.15986@stu.upes.ac.in>
2024-09-24 22:32:04 +0530

    exception handling

---
+++ trys.java
@@ -0,0 +1,18 @@
+class trys{
+
+public static void main(String args[]) {
+
+String obj = "hello";
+
+String obj1 = "UPES";
+
+String obj2 = new String("UPES");
+
+obj2 = obj;
+
+System.out.println(obj + " " + obj2);
+
+
+
+
+}}
